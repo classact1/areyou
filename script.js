cb6c81f10645031c58fe95fef6e4f5194fd9e181
classact1 <jaczewskipawel.pl@gmail.com>
2017-08-01 08:45:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const noButton = document.querySelector('.no');
+const yesButton = document.querySelector('.yes');
+const video = document.querySelector('.video');
+const container = document.querySelector('.container');
+const body = document.querySelector('body');
 
-function move(){
-   noButton.style.position = 'absolute';
-   noButton.style.top = Math.floor(Math.random()*window.innerHeight)+'px';
-   noButton.style.left = Math.floor(Math.random()*window.innerWidth)+'px';
+let escaped = 0;
+
+function escape(){
+   if(escaped < 4){
+      noButton.style.position = 'absolute';
+      noButton.style.top = Math.floor(Math.random()*(window.innerHeight - noButton.offsetHeight))+'px';
+      noButton.style.left = Math.floor(Math.random()*(window.innerWidth - noButton.offsetWidth))+'px';
+      escaped++;
+   }
+   else
+      changeBtnText();
 }
 
-noButton.addEventListener('mouseenter', move);
+function changeBtnText(){
+   noButton.innerText = 'YES';
+   yesButton.innerText = 'NO';
+}
+
+function triggerVideo(){
+   container.style.display = 'none';
+   video.style.display = 'block';
+   body.style.backgroundColor = '#f740e1';
+   video.src += '?autoplay=1';
+}
+
+noButton.addEventListener('mouseenter', escape);
+noButton.addEventListener('click', triggerVideo);
+
